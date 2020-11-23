49aa67b7e647f6014fc657f081714b510f14fa54
Rockstics <35019240+Rockstics@users.noreply.github.com>
2020-11-23 18:03:23 +0800

    Update settings.example.py
    修改SECRET_KEY

--- LJlogin_sgin/settings.example.py
+++ LJlogin_sgin/settings.example.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'y6opftbck5783k86%j5!jh)8-$&+b6+4pfdggg8&)'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
